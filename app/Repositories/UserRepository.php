665fcc6b6da142784119bab9382c324d62dacd83
Arthur Bandeira <arthurbandeirafc@gmail.com>
2024-07-17 13:00:15 -0300

    :sparkles: feat: Adicionando a rota, resources, controllers e models para a api dos usuários no laravel 11

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repositories;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+class UserRepository
+{
+    public function __construct(protected User $user)
+    {
+
+    }
+
+    public function getPaginate(int $totalPerPage = 15, int $page = 1, string $filter = ''): LengthAwarePaginator
+    {
+        return $this->user->where(function ($query) use ($filter) {
+            if ($filter !== '') {
+                $query->where('name', 'like', '%{$filter}%');
+            }
+        })->paginate($totalPerPage, ['*'], 'page', $page);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\Api\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/', fn () => response()->json(['message' => 'ok1']));
